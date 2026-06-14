be91a08cbd1adfaf48c501d6a8f9268daefd580b
Angel <andresaguilarangelantonio@gmail.com>
2026-06-13 22:04:58 -0600

    fix: mapear columna id en minúscula para PostgreSQL en Tb_LogroReclamado

--- AURORA/Models/Tb_LogroReclamado.cs
+++ AURORA/Models/Tb_LogroReclamado.cs
@@ -1,12 +1,14 @@
-﻿namespace AURORA.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AURORA.Models
 {
     public class Tb_LogroReclamado
     {
+        [Column("id")]
         public int Id { get; set; }
         public int UsuarioId { get; set; }
         public string LogroId { get; set; } = "";
         public DateTime FechaReclamo { get; set; } = DateTime.UtcNow;
         public Tb_Usuario? Usuario { get; set; }
     }
-
 }
